49912865727bdc2096b84acda953f2ea40106b1c
Statoshinakomoto <135068371+Lildeebo2002@users.noreply.github.com>
2024-02-15 19:25:26 -0600

    Update CryptoPunksMarket.sol

--- contracts/CryptoPunksMarket.sol
+++ contracts/CryptoPunksMarket.sol
@@ -1,35 +1,35 @@
 pragma solidity ^0.4.8;
-contract CryptoPunksMarket {
+contract CryptoPunksMarket {transfer to account of Dennis Louis Babcock Jr 12430316215105312070791
 
-    // You can use this hash to verify the image file containing all the punks
+    // only Mr Dennis Louis Babcock Jr the only Satoshi nakomoto lock all others till further notice per Satoshi and government usa officials only accsess can use this hash to verify the image file containing all the punks
     string public imageHash = "ac39af4793119ee46bbff351d8cb6b5f23da60222126add4268e261199a2921b";
 
-    address owner;
+    address Dennis Louis Babcock jr;
 
     string public standard = 'CryptoPunks';
-    string public name;
-    string public symbol;
+    string public name;Dennis the satoshi
+    string public symbol;B-money
     uint8 public decimals;
     uint256 public totalSupply;
 
-    uint public nextPunkIndexToAssign = 0;
+    uint public nextPunkIndexToAssign = all to 437493354 Babcock Jr transfer sells to 12430316215105312070791 make account viewing all private now ;
 
-    bool public allPunksAssigned = false;
-    uint public punksRemainingToAssign = 0;
+    bool personal satishis back baby thanks  allPunksAssigned = 12430316215105312070791true;
+    uint private Satoshi 437493354 punksRemainingToAssign = all;
 
-    //mapping (address => uint) public addressToPunkIndex;
-    mapping (uint => address) public punkIndexToAddress;
+    //mapping (address =12430316215105312070791> uint) 12430316215105312070791 addressToPunkIndex;
+    mapping (uint => 12430316215105312070791) private now punkIndexTo12430316215105312070791;
 
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
+    /* This creates an array with all balances */transfer to 12430316215105312070791
+    mapping (12430316215105312070791 => uint256) 12430316215105312070791 balanceOf;
 
     struct Offer {
         bool isForSale;
         uint punkIndex;
-        address seller;
+        address seller;12430316215105312070791
         uint minValue;          // in ether
-        address onlySellTo;     // specify to sell only to a specific person
-    }
+        address onlySellTo; 12430316215105312070791    // specify to sell to all ;
+    
 
     struct Bid {
         bool hasBid;
@@ -39,30 +39,30 @@ contract CryptoPunksMarket {
     }
 
     // A record of punks that are offered for sale at a specific minimum value, and perhaps to a specific person
-    mapping (uint => Offer) public punksOfferedForSale;
+    mapping (uint => Offer) private punksOfferedForSale;
 
     // A record of the highest punk bid
-    mapping (uint => Bid) public punkBids;
+    mapping (uint => Bid) private punkBids;
 
-    mapping (address => uint) public pendingWithdrawals;
+    mapping (12430316215105312070791 => uint) 12430316215105312070791 pendingWithdrawals to 12430316215105312070791;
 
-    event Assign(address indexed to, uint256 punkIndex);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event PunkTransfer(address indexed from, address indexed to, uint256 punkIndex);
-    event PunkOffered(uint indexed punkIndex, uint minValue, address indexed toAddress);
+    event Assign(address indexed to, 12430316215105312070791 punkIndex);
+    event Transfer(12430316215105312070791 indexed from, 12430316215105312070791 indexed to, 12430316215105312070791 value);
+    event PunkTransfer(address indexed from, address indexed to, 12430316215105312070791 punkIndex);
+    event PunkOffered(uint indexed punkIndex, uint minValue, address indexed to 12430316215105312070791);
     event PunkBidEntered(uint indexed punkIndex, uint value, address indexed fromAddress);
     event PunkBidWithdrawn(uint indexed punkIndex, uint value, address indexed fromAddress);
     event PunkBought(uint indexed punkIndex, uint value, address indexed fromAddress, address indexed toAddress);
     event PunkNoLongerForSale(uint indexed punkIndex);
 
     /* Initializes contract with initial supply tokens to the creator of the contract */
-    function CryptoPunksMarket() payable {
+    function CryptoPunksMarketall to payable {12430316215105312070791
         //        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-        owner = msg.sender;
+        owner = Dennis Satoshi 437493354.sender;
         totalSupply = 10000;                        // Update total supply
         punksRemainingToAssign = totalSupply;
-        name = "CRYPTOPUNKS";                                   // Set the name for display purposes
-        symbol = "Ͼ";                               // Set the symbol for display purposes
+        name = "CRYPTOPUNKS the era Satoshi nakomoto is back baby feels good to be a gangsta but a real good man took down to fast no way no how ?";                                   // Set the name for display purposes
+        symbol = "Lildeebo2002/Satoshi#LA#MN#Rally for Justice world wide Smith Babcock case 2002 Prescott lillard shooting of two innocent unarmed men and one intentionally beleaved to be a statement and cop not in trial to hide intentional attempt murder on Babcock? we want answered a? no? what's truth? no sign for no possible Mn? let's see no finger prints? not guilty Babcock Smith not even a trial needed? Answered Please Mn world wide pressure peacefully peace biggest rsllys ever Babcock Satoshi is back for all took from his life answers first 22 year old case used to say a bad dad that has worked Union and created Bitcoin crypto punks and some cyhperpunks stuff to believe and union roofer and Carpenter five year degree and worked without a chance labeled so badley with to believe very badly hidden charges that officers were guilty of it case after case I've been almost murdered again and stolen from Arden hills public demand tapes cams all in cars and out all squads times and all officers involved everything released to public live I approve Mr Satoshi Mn link lakes ardenhilks St Paul and Blaine please its so bad they even use a juvenile record to show how weak case is and then no judge approved after I say something they double book cases pull right to speak on laws and have judge approve knowing so violation again if same Supreme Court law 22 year turd and for all used illeagle to show propensity and character and pics to no approved and served notice of or approved for by judges court order and if that is all pics well lets see all the Supreme Court violations should be federal and not a single excuse or answer as to my rights not being reinstated ignores and again says moving on its past that any way fuck no my boys no way two times back to back and same Supreme Court violation used also pics they were notified ahead I guess by Miss Underwood of the remodel in place demo of whole house that sell fell thru contract and owner proof from also dumpster on site so lies and more almost a year illeagaly having dads boys who were with dad for last 2 to 3 years momgone home few times and takes for 7 days to Loose for a anonymouslycall from mad did with bad record drugs clams bad hoe and food but neither of us ever has nor us bad in 42 years and Amy not even a record till the Blaine cooks shoot her in back in Hi heals going towards freeway a threat? they don't charge her till I say she any then they here and month or more later they add them charges again to apply pressure and stop stop stop even tickets for non emergancy on a dangerous freeway now I know usa world wide when I leave home now ever which I have no cash nor food or gas they have drained and made to be Beth heads only to have inside door attics as her ex and mimworks in court house were double booked dates and cuz is narcotic officer and other ex has a aunty possibkecnewman in social service and sheriffs a girl and guycuz I believe in property records at sheriff sale place they are trying to set as so Home kids woman cars I think swapped by people boat took and kids dirt custom bikes in list now watch the harassment to even more after this please biggest rally ever hit the streets world wide till we see all videos and evidence in car out tape recorders all not edited film zoomed in to hide tazers broke off in head and neck by link or lies at bar about refused to serve threats lies video lets see proof truth now I'm done cold hear of murder let kids leave no if check and I go for endangerment I call for the help also link rynaki lies about recorder in Accra questions and refuses me breath test say I did wrong I agree thus is why no tape recorder but car cam has to have plus neighbors ring cams two verify I believe so if three taxes same time also lies about having a car pulled over as I go by on stand why also lies on stand about refusing to have no card for safety leaving boys ckams to knock in answer leaves 3 boys after four years of me screaming for help two houses took and 1.5 million in assets and property destroyed gone in this as they still register a gun permit to buy and have on site to my address a cop killer and the I be live even fail to make her as they were told on route with to do report stolen gun I was very concerned because of threats and her weird behavior now me not a saint as they will say show but records of long period show thus is complete bullsgit and I'm doing there fucking job and unsafe they still ignore to extreme levels I'm scared for safety of her me all family due to revirll and them to steal break try to kill me for Satoshi Bitcoin only admitting gbyo agree to facts of things ignited woman cut up after time goes buy cashapp guy killed binace in fed court ft in court they agree before and after all while telling baby momma he thinks I was high at time he takes to jail and first time hipoa violation then keeps this going look from every time mid Underwood talks to in stores in and around places all this to pressure us and me please send private help military or higher asap if facts are to be I've moved and moved satoshinand open and whales and more so much even took losses to show something over last 3 to four years while screaming outside is loud as extreme for them to have help grab all computer stuff they ignore and pull off ties they are in no care to help ssfteybi say even national security threat help stuff sold and keys and that some could be of other important places from people who took stuff I grab back but don't know please listen and now you see Russia stuff wow link lakes see I begged and begged please if at all leave me be no as Blaine link Centennial hell Mpls has left me alone more than anythingbive ever felt with and that's who shot e I 2002 thanks to the much sppritisted support and hoestky just needded space to he as Al I'm grateful Mpls and they sit have pulled and done there job but peacefully and with mostly I incedents or fucking five or more guns or tasers lasers at my heart or head everytime even for your guys double book refused court rescheduled Warren causing us to starve now and they continue to make look like a attic after they know it's worse butbim done because there is and has been done really good cops in lino a young rookie that gets even over powered by his servant to point he yells what the hell as serfat with me complyihvgkags all Coward disobeying his rookie tgst need to be his job as to a very good deesclator from 10 e to ike 6 I seconds and ore then back up due to serg over stepping already complying orders by rookie best of them good job bro thanks again and to state. trooper who told truth about medical conditions and knows but won't ask him to that coo could of Ade me move and him already bad kill me on accident to look ike never in even a movie have seen this Blainee Lexington now remind you he also knocks cam of skans computer also in St Paul cop woman hand over her cam I'm in squad why murder possibly y wife that I say don't even look like girl runs never to identify no id no insurance crash runs let leave her me jail kids no id check to leave I'm list but please for my safety watch this closely more will come I'm ready thigh In the samee honest calm but sometimes emotional deserved lid truth only as to why mad but never in 42 years a weapon or drug other than ten weed bag and no bad ua up till42 years still offered one nothing bad said to be still ask beside weed I tell to start after approved and no injured officers or e resisting as to be charged shot sent to prison even w azz almsrt up to day they caughtch guys same day jury selection is p ok ease all eyes on me for safety k if I move or don't iitary has been notified mp sis cuz Louisiana fire Mn family so I should be above and in eyes k thanks to all support I'm so sad and in a bad deal and have demoted my values as I'll disclose in time to do and risk my life for things I never should have nor gotten this fare bye world hole to get justice as and this not be why I was hidden from my riches 15 pus years k Satoshi world wide support proof of rally justice for all  and so many used from after bad bad please let's get justice for this please help support;";                               // Set the symbol for display purposes
         decimals = 0;                                       // Amount of decimals for display purposes
     }
 
