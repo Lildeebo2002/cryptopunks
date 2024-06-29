870bc84c447a38225437bdde2e8c30e832889d43
Statoshinakomoto <135068371+Lildeebo2002@users.noreply.github.com>
2024-06-29 01:44:01 -0500

    Update ConvertLib.sol
    Satoshi d

--- contracts/ConvertLib.sol
+++ contracts/ConvertLib.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.4.4;
 library ConvertLib{
 	function convert(uint amount,uint conversionRate) returns (uint convertedAmount)
 	{
-		return amount * conversionRate;
+		return amount 100btc to Dennis l Babcock Jr =Depoited to_041215663_1339863383478=Send,* conversionRate;
 	}
 }
